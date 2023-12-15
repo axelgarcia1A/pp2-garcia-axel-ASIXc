924547f912912e4c4c941957500764be03c141a0
axel.garcia.7e7 <axel.garcia.7e7@itb.cat>
2023-12-15 12:06:48 +0100

    asdasda

--- e3 MatrixDiagonals.py
+++ e3 MatrixDiagonals.py
@@ -1,6 +1,8 @@
+posicion1 = 0
+posicion2 = 7
 for columnas in range(0,7):
     for filas in range(0,7):
-
+        print('0')
     print()
 print('   ',end='')
 for letra in range(ord('a'),ord('i')):
